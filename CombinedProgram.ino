edbd155ca1401d4b0747a977b25c0d950a95682a
maryelichtenwalner <94236239+maryelichtenwalner@users.noreply.github.com>
2022-04-04 16:39:57 -0400

    Add files via upload

--- CombinedProgram.ino
+++ CombinedProgram.ino
@@ -1,15 +1,48 @@
 /*
- * Combined Program: Measures pressure, temperature, humidity
+ * Combined Program: Takes measurements from each sensor.
+ *      Includes: 
+ *      two pressure sensors
+ *      three thermocouples with MAX31855 thermocouple amplifier breakout boards
+ *      one AHT20 temperature/humidity sensor
+ *      one EK1245 Gikfun obstacle avoidance infrared sensor module for shaft speed measurement
  * 
  * Written by Mary Lichtenwalner
  * 
- * March 22, 2022
+ * Last Update: April 4, 2022
  */
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Section 0. Arduino Uno connections
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ /*
+  * All sensors
+  *   Connect VCC to 5V, GND to ground
+  *   
+  * MAX31855 Thermocouple amplifier breakout board
+  *   Thermocouple 1: Compressed air, DO to pin 3, CS to pin 4, CLK to pin 5
+  *   Thermocouple 2: Chamber, DO to pin 3, CS to pin 6, CLK to pin 5
+  *   Thermocouple 3: Exhaust, DO to pin 3, CS to pin 7, CLK to pin 5
+  *   
+  * Pressure transducers
+  *   Transducer 1: Ambient air, red to VCC, black to GND, yellow to pin A1
+  *   Transducer 2: Compressed air, red to VCC, black to GND, yellow to pin A2
+  *   
+  * AHT20 temperature/humidity sensor
+  *   SDA to SDA, SCL to SCL
+  *   
+  * Gikfun infrared sensor module
+  *   red to VCC, black to GND, yellow to pin 2
+  */
 
 // Libraries to include
 #include <SPI.h>
 #include "Adafruit_MAX31855.h"
 #include <Adafruit_AHTX0.h>
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Section 1. Set up sensors
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 //***********************************************************************
 // Set up thermocouples
@@ -24,15 +57,11 @@ Adafruit_MAX31855 thermocouple_1(MAXCLK, MAXCS1, MAXDO);
 
 // Second thermocouple (reuse DO and CLK)
 #define MAXCS2  6
-//#define MAXDO2  2
-//#define MAXCLK2 9
 
 Adafruit_MAX31855 thermocouple_2(MAXCLK, MAXCS2, MAXDO);
 
 // Third thermocouple (reuse DO and CLK)
 #define MAXCS3  7
-//#define MAXDO3  8
-//#define MAXCLK3 10
 
 Adafruit_MAX31855 thermocouple_3(MAXCLK, MAXCS3, MAXDO);
 
@@ -52,11 +81,14 @@ double pressureReading2 = 0.0;
 // Set up temperature/humidity sensor
 //***********************************************************************
 
+// Initialize sensor
 Adafruit_AHTX0 aht;
 
 //***********************************************************************
 // Set up IR sensor
 //***********************************************************************
+
+// Variables used in rpm calculation
 float value=0;
 float rev=0;
 int rpm;
@@ -68,17 +100,18 @@ void isr() //interrupt service routine
 rev++;
 }
 
-//***********************************************************************
-// Set up program
-//***********************************************************************
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Section 2. Set up program
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 void setup() {
-  // Set the pressure pins as input pins
+  
+  // Designate two analog pins as input for the pressure transducers
   pinMode(pressurePin1, INPUT);
   pinMode(pressurePin2, INPUT);
 
   // Infrared sensor goes in digital pin 2
   attachInterrupt(0,isr,RISING);  //attaching the interrupt
-  //pinMode(2, INPUT);
   
   // Initialize baud rate
   Serial.begin(9600);
@@ -91,21 +124,21 @@ void setup() {
   thermocouple_3.begin();
 
   // Initialize temperature/humidity sensor
-//  if (! aht.begin()) {
-//    Serial.println("Could not find AHT? Check wiring");
-//    while (1) delay(10);
-//  }
-//  Serial.println("AHT10 or AHT20 found");
-
   aht.begin();
   delay(10);
 
 }
 
-//***********************************************************************
-// Part to run on loop
-//***********************************************************************
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Section 3. Part that runs on a loop
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 void loop() {
+
+  //*********************************************************************
+  // Goes through and takes a reading from each sensor
+  //*********************************************************************
+  
   // Read thermocouples in Fahrenheit
   String temp1 = String(thermocouple_1.readFahrenheit());
   String temp2 = String(thermocouple_2.readFahrenheit());
@@ -115,25 +148,28 @@ void loop() {
   pressureReading1 = analogRead(pressurePin1);
   pressureReading2 = analogRead(pressurePin2);
 
-  // Convert pressures to proper units
-  float voltage1 = (pressureReading1*5.0)/1023.0;   
-  //float pressure_pascal1 = (3.0*(voltage1-0.115))*1000000.0;                       //calibrate here
-  float pressure_pascal1 = (3.0*(voltage1-0.135))*1000000.0;                       //calibrate here
+  // Convert pressures to proper units, want output in psi
+  // Pressure transducer 1
+  float voltage1 = (pressureReading1*5.0)/1023.0;
+  float calibration = 0.135;                                                            // Calibrate here. Adjust so that ambient pressure is correct.
+  float pressure_pascal1 = (3.0*(voltage1-calibration))*1000000.0; 
   float pressure_bar1 = pressure_pascal1/10e5;
   float pressure_psi1 = pressure_bar1*14.5038;
 
-  float voltage2 = (pressureReading2*5.0)/1023.0;   
-  //float pressure_pascal2 = (3.0*(voltage2-0.13))*1000000.0;                       //calibrate here
-  float pressure_pascal2 = (3.0*(voltage2-0.14))*1000000.0;                       //calibrate here
+  // Pressure transducer 2
+  float voltage2 = (pressureReading2*5.0)/1023.0;
+  float calibration = 0.14;                                                             // Calibrate here. Adjust so that ambient pressure is correct.
+  float pressure_pascal2 = (3.0*(voltage2-calibration))*1000000.0;
   float pressure_bar2 = pressure_pascal2/10e5;
   float pressure_psi2 = pressure_bar2*14.5038;
 
+  // Final output from both pressure transducers
   String pressure1 = String(pressure_psi1);
   String pressure2 = String(pressure_psi2);
 
   // Read temperature/humidity sensor
   sensors_event_t humidity, temp;
-  aht.getEvent(&humidity, &temp); // Populate temp and humidity objects with fresh data
+  aht.getEvent(&humidity, &temp);                                                       // Populate temp and humidity objects with fresh data
 
   // Read infrared shaft speed sensor
   detachInterrupt(0);           //detaches the interrupt
@@ -142,15 +178,25 @@ void loop() {
   oldtime=millis();             //saves the current time
   rev=0;
 
-  // Output all values to serial monitor
-  String output = temp1 + " " + temp2 + " " + temp3 + " " + pressure1 + " " + pressure2 + " " + temp.temperature + " " + humidity.relative_humidity + " " + rpm;
-  //String output = temp1 + " " + temp2 + " " + temp3 + " " + pressure1 + " " + pressure2 + " " + "0" + " " + "0" + " " + rpm;
+  //*********************************************************************
+  // Send the output to the serial port
+  //*********************************************************************
 
+  // Combine all measurements into one string with spaces in between values
+  String output = temp1 + " " + temp2 + " " + temp3 + " " + pressure1 + " " + pressure2 + " " + temp.temperature + " " + humidity.relative_humidity + " " + rpm;
+
+  // Output single string containing each measurement
   Serial.println(output);
+
+  // Uncomment following section to see error codes if thermocouples are reading NAN
 //  Serial.print(thermocouple_1.readError());
 //  Serial.print(thermocouple_2.readError());
 //  Serial.print(thermocouple_3.readError());
 //  Serial.print('\n');
+
+  //*********************************************************************
+  // Delay before next reading and restart rpm interrupt
+  //*********************************************************************
   
   // Delay one second before next reading
   delay(1000);
